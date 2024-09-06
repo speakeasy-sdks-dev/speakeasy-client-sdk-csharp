fbde8d2274f1a8091d6385b4e30e9b8563af624b
regression-test-executor <regression-test-executor@speakeasy.com>
2024-09-06 13:54:23 +0000

    openapi-generation@fd889444afe5ee65e12dd1a1ec7c3d9f07557ea6

--- SpeakeasySDK/Embeds.cs
+++ SpeakeasySDK/Embeds.cs
@@ -57,9 +57,9 @@ namespace SpeakeasySDK
         public SDKConfig SDKConfiguration { get; private set; }
         private const string _language = "csharp";
         private const string _sdkVersion = "5.9.27";
-        private const string _sdkGenVersion = "2.382.0";
+        private const string _sdkGenVersion = "2.411.4";
         private const string _openapiDocVersion = "0.4.0 .";
-        private const string _userAgent = "speakeasy-sdk/csharp 5.9.27 2.382.0 0.4.0 . SpeakeasySDK";
+        private const string _userAgent = "speakeasy-sdk/csharp 5.9.27 2.411.4 0.4.0 . SpeakeasySDK";
         private string _serverUrl = "";
         private ISpeakeasyHttpClient _client;
         private Func<SpeakeasySDK.Models.Shared.Security>? _securitySource;
